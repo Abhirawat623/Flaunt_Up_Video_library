2603d648eea01a4d05ddc53f3631320b023019fb
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-09-12 22:49:17 +0530

    videos controller added

---
+++ src/backend/controllers.js/videoLibrary.js
@@ -0,0 +1,44 @@
+import { Response } from "miragejs";
+
+
+export const getAllVideosHandler = function () {
+  try {
+    return new Response(200, {}, { videos: this.db.videos });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+/**
+ * This handler handles uploads a new video to the db.
+ * send POST Request at /api/user/videos/
+ * */
+
+// TODO: postVideoHandler
+
+/**
+ * This handler handles gets all videos in the db.
+ * send GET Request at /api/user/videos/:videoId
+ * */
+
+export const getVideoHandler = function (schema, request) {
+  const { videoId } = request.params;
+  try {
+    const video = schema.videos.findBy({ _id: videoId }).attrs;
+    return new Response(200, {}, { video });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
