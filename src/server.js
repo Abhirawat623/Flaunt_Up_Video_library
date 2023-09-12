ffa23a75dc67e82f6507b142e9b2b2cdd195216b
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-09-12 21:37:34 +0530

    Environment added

--- src/server.js
+++ src/server.js
@@ -1,17 +1,34 @@
-  // Runs on the start of the server
-  seeds(server) {
-    server.logging = false;
-    videos.forEach((item) => {
-      server.create("video", { ...item });
-    });
-  
-    users.forEach((item) =>
-      server.create("user", {
-        ...item,
-        likes: [],
-        watchlater: [],
-        history: [],
-        playlists: [],
-      })
-    );
-  },
+  export function makeServer({ environment = "development" } = {}) {
+  return new Server({
+    serializers: {
+      application: RestSerializer,
+    },
+    environment,
+    // TODO: Use Relationships to have named relational Data
+    models: {
+      video: Model,
+      category: Model,
+      user: Model,
+      like: Model,
+      history: Model,
+      playlist: Model,
+      watchlater: Model,
+    },
+
+    // Runs on the start of the server
+    seeds(server) {
+      server.logging = false;
+      videos.forEach((item) => {
+        server.create("video", { ...item });
+      });
+      categories.forEach((item) => server.create("category", { ...item }));
+      users.forEach((item) =>
+        server.create("user", {
+          ...item,
+          likes: [],
+          watchlater: [],
+          history: [],
+          playlists: [],
+        })
+      );
+    },
