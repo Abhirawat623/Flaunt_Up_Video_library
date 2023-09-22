713df427e04322b34c63bf1e17b30d2134f69925
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-09-22 17:08:25 +0530

    mongo db  added

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
 const app = express();
-
 const PORT=8080;
+//mongoose
+const mongoose = require('mongoose')
+//dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+//cors
+const cors = require('cors');
+app.use(cors());
+//db
+const connectdb = require('./configuration/dbconfig');
+connectdb();
 
 app.use(express.json())
 
@@ -16,6 +25,11 @@ app.get("/",(req,res)=>{
 app.use('/api/videos',videoRouter)
 
 
-app.listen(process.env.PORT ||PORT,()=>{
-    console.log('server is running')
+
+
+mongoose.connection.once("open",()=>{
+    console.log('connected to db');
+    app.listen(process.env.PORT || PORT,()=>{
+        console.log("server is comnected")
+    })
 })
