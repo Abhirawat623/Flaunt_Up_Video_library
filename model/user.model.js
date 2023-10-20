a8faeb2bbd3107d8397dbc063ada51fb67a3390a
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-10-20 23:43:28 +0530

    Update user.model.js

--- model/user.model.js
+++ model/user.model.js
@@ -6,9 +6,6 @@ const userSchema = new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true}
    
-},
-{
-    timestamps:true
 });
 
 const User = mongoose.model("User",userSchema);
