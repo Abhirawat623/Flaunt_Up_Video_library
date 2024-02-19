37dd7957321f72fed5152842dc5c305d6e33d12a
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-02-19 23:09:48 +0530

    Update categories.js

--- db/categories.js
+++ db/categories.js
@@ -1,36 +1,31 @@
+import { v4 as uuid } from "uuid";
 
 
-const categories ={data: [
-  {
-  
-    categoryName: "Intelligence",
-    description: `Three things:
-    - Strength/Aesthetics/Fitness- Knowledge/Wisdom/Intelligence- Social Skills/Machiavellianism/Game
-    You want to work on all three of them.
-    - You do not want to be 1/3.- You do not want to be 2/3.- You want to be 3/3.
-    Don't severely neglect one.
-    ~Harsh Strongman`,
-  },
-  {
-  
-    categoryName: "Social Skills",
-    description: `Three things:
-    - Strength/Aesthetics/Fitness- Knowledge/Wisdom/Intelligence- Social Skills/Machiavellianism/Game
-    You want to work on all three of them.
-    - You do not want to be 1/3.- You do not want to be 2/3.- You want to be 3/3.
-    Don't severely neglect one.
-    ~Harsh Strongman`,
-  },
-  {
-  
-    categoryName: "Strength",
-    description: `Three things:
-      - Strength/Aesthetics/Fitness- Knowledge/Wisdom/Intelligence- Social Skills/Machiavellianism/Game
-      You want to work on all three of them.
-      - You do not want to be 1/3.- You do not want to be 2/3.- You want to be 3/3.
-      Don't severely neglect one.
-      ~Harsh Strongman`,
-  },
-]};
 
-exports.module = categories;
+export const categories = [
+  {
+    _id: uuid(),
+    categoryName: "All",
+    videoCategory: "all"
+  },
+  {
+    _id: uuid(),
+    categoryName: "Trending",
+    videoCategory: "trending"
+  },
+  {
+    _id: uuid(),
+    categoryName: "Smart Wearables",
+    videoCategory: "wearables"
+  },
+  {
+    _id: uuid(),
+    categoryName: "Smart Phones",
+    videoCategory: "phone"
+  },
+  {
+    _id: uuid(),
+    categoryName: "Laptops",
+    videoCategory: "laptop"
+  }
+];
